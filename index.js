b58720db2ff981ddc04a594b2dc3373a93b0568b
E021668 <jal7703@gmail.com>
2022-08-31 10:57:06 +0200

    1 file modified

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function highAndLow(numbers){
   highAndLow("1 2 -3 4 5"); // return "5 -3"
   highAndLow("1 9 3 4 -5"); // return "9 -5"*/
 
-  var res = 1 + 1;
-  return res;
+  var arr = numbers.split(" ");
+  return arr;
 }
 var res = highAndLow("1 2 3 4");
 document.write(res);
