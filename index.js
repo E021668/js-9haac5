884de172ff08b32c1b2174d7639bbf21d377de59
E021668 <jal7703@gmail.com>
2022-08-31 10:53:19 +0200

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+function highAndLow(numbers){
+  /*Test string In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+  Examples
+  highAndLow("1 2 3 4 5");  // return "5 1"
+  highAndLow("1 2 -3 4 5"); // return "5 -3"
+  highAndLow("1 9 3 4 -5"); // return "9 -5"*/
+
+  var res = 1 + 1;
+  return res;
+}
+var res = highAndLow("1 2 3 4");
+document.write(res);
